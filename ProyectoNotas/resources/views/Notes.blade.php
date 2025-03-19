d16331a79d4e4bc216aa9e4b249423205d049be5
Carlos Eduardo Merino Ventura <merinoventura123@gmail.com>
2025-03-18 20:19:36 -0600

    Notas
    Notas

--- ProyectoNotas/resources/views/Notes.blade.php
+++ ProyectoNotas/resources/views/Notes.blade.php
@@ -6,6 +6,7 @@
     <title>Notes Dashboard</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+<body class="bg-gray-100 p-6 ">
     <div class="max-w-6xl mx-auto ">
         <div class="flex justify-between items-center mb-6">
             <h1 class="text-2xl font-bold">Notes</h1>
