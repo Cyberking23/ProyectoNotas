be28feae314e745236158e7b42b86dc781079c15
Gerardo Saz <gerardo.saz120@gmail.com>
2025-03-23 18:30:48 -0600

    agregar middleware de auth

--- ProyectoNotas/routes/web.php
+++ ProyectoNotas/routes/web.php
@@ -1,26 +1,46 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
-Route::get('/', function () {
-    return view('login');
-})->name('login');
-
-Route::get('/notes', function () {
-    return view('Notes');
-})->name('notes.index');
-
-Route::get('/formnotes', function () {
-    return view('NotesForm');
-})->name('notes.form');
-
-Route::get('/register', function () {
-    return view('Register');
-})->name('register.form');
+use Illuminate\Support\Facades\Route;
 
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::post('/login', [AuthController::class, 'login'])->name('login.post');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware('guest')->group(function () {
+    Route::get("/", function(){
+        return redirect("/login"); 
+    });
+    Route::get('/login', function () {
+        return view('login');
+    })->name('login');
+
+    Route::get('/register', function () {
+        return view('register');
+    })->name("register");
+
+    Route::post('/login', [AuthController::class, 'login'])->name("login.post");
+    Route::post('/register', [AuthController::class, 'register'])->name("register.post");
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/notes', function () {
+        return view('Notes');
+    })->name('notes.index');
+
+    Route::get('/formnotes', function () {
+        return view('NotesForm');
+    })->name('notes.form');
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
